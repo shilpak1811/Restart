56636e61b804ba2a53094938d5bd051da8e75ed7
Shilpa Kumari <shilpa.kumari@gojek.com>
2022-12-31 15:04:39 +0530

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Shilpa Kumari on 31/12/22.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
